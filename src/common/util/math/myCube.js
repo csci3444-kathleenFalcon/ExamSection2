21d8e086175b2f2320de1a9ccecc2fcfd80f0118
kfalcon95 <kfalcon@student.fdu.edu>
2017-11-15 18:45:25 -0500

    updated input formats

--- src/common/util/math/myCube.js
+++ src/common/util/math/myCube.js
@@ -2,6 +2,8 @@ module.exports = {
     toCube: function(inpNum) {
         if (typeof inpNum === "number")
             return inpNum * inpNum * inpNum;
+        else if (typeof inpNum === "string" && Number(inpNum) === "number")
+            return Number(inpNum) * Number(inpNum) * Number(inpNum);
         else
             return "NaN";
     }
--- src/common/util/math/mySquare.js
+++ src/common/util/math/mySquare.js
@@ -2,6 +2,8 @@ module.exports = {
     toSquare: function(inpNum) {
         if (typeof inpNum === "number")
             return inpNum * inpNum;
+        else if (typeof inpNum === "string" && Number(inpNum) === "number")
+            return Number(inpNum) * Number(inpNum);
         else
             return "NaN";
     }
