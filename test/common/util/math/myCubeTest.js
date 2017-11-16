6b64036bf57d9c1753d79ef5dccacb2fe9584e37
kfalcon95 <kfalcon@student.fdu.edu>
2017-11-15 19:23:07 -0500

    updated unit testing

--- test/common/util/math/myCubeTest.js
+++ test/common/util/math/myCubeTest.js
@@ -1,1 +1,35 @@
-//copy from ex7
+// NOTE this is following BDD
+
+// const assert = require('assert');
+// NOTE using chai for assertions instead of assert that comes with nodeJS 
+const assert = require('chai').assert;
+const myCube = require('./src/common/util/math/myCube');
+
+const num1 = 5;
+const num1String = "5";
+const num1NaN = "$";
+const num1Cube = 125;
+
+describe('myCube', function() {
+    describe('toCube', function() {
+        it('toCube(' + num1 + ') should return the value of "' + num1Cube + '"', function() {
+            let result = myCube.toCube(num1);
+            assert.equal(result, num1Cube);
+        });
+
+        it('toCube(' + num1String + ') should return the value of "' + num1Cube + '"', function() {
+            let result = myCube.toCube(num1String);
+            assert.equal(result, num1Cube);
+        });
+
+        it('toCube(' + num1NaN + ') should return the value of "NaN"', function() {
+            let result = myCube.toCube(num1NaN);
+            assert.equal(result, "NaN");
+        });
+
+        it('toCube(' + num1 + ') should return type number', function() {
+            let result = myCube.toCube(num1);
+            assert.typeOf(result, 'number');
+        });
+    });
+});
--- test/common/util/math/mySquareTest.js
+++ test/common/util/math/mySquareTest.js
@@ -1,1 +1,35 @@
-//copy from ex7
+// NOTE this is following BDD
+
+// const assert = require('assert');
+// NOTE using chai for assertions instead of assert that comes with nodeJS 
+const assert = require('chai').assert;
+const myCube = require('./src/common/util/math/mySquare');
+
+const num1 = 5;
+const num1String = "5";
+const num1NaN = "$";
+const num1Square = 25;
+
+describe('mySquare', function() {
+    describe('toSquare', function() {
+        it('toSquare(' + num1 + ') should return the value of "' + num1Square + '"', function() {
+            let result = mySquare.toSquare(num1);
+            assert.equal(result, num1Square);
+        });
+
+        it('toSquare(' + num1String + ') should return the value of "' + num1Square + '"', function() {
+            let result = mySquare.toSquare(num1String);
+            assert.equal(result, num1Square);
+        });
+
+        it('toSquare(' + num1NaN + ') should return the value of "NaN"', function() {
+            let result = mySquare.toSquare(num1NaN);
+            assert.equal(result, "NaN");
+        });
+
+        it('toSquare(' + num1 + ') should return type number', function() {
+            let result = mySquare.toSquare(num1);
+            assert.typeOf(result, 'number');
+        });
+    });
+});
