a81fac49f701b3a84e4375bd0e443b23a188a87b
kfalcon95 <kfalcon@student.fdu.edu>
2017-11-20 00:12:08 -0500

    added myPowers code and unit tests

--- src/common/util/math/myPowers.js
+++ src/common/util/math/myPowers.js
@@ -1,1 +1,39 @@
-console.log("powers");
+module.exports = {
+    toSquare: function(inpNumber) {
+        const mySquare = require('./mySquare');
+        return (mySquare.toSquare(inpNumber));
+    },
+    toCube: function(inpNumber) {
+        const myCube = require('./myCube');
+        return (myCube.toCube(inpNumber));
+    }
+};
+
+/* raise a number to any power
+module.exports = {
+    toPowers: function(inpNumber, power)
+    {
+        if (isNaN(inpNum))
+            return "NaN";
+        else if (typeof inpNum === "number" && typeof power === "number")
+            return (Math.pow(inpNum, power));
+        else
+            return ("Invalid input/selection.");
+    }
+}
+*/
+
+/* square or cube by command
+module.exports = {
+    toPowers: function(inpNumber, cmd) {
+        if (toLowerCase(cmd) == "square") {
+            const mySquare = require('./mySquare');
+            return (mySquare.toSquare(inpNumber));
+        } else if (toLowerCase(cmd) == "cube") {
+            const myCube = require('./myCube');
+            return (myCube.toCube(inpNumber));
+        } else
+            return ("Invalid input/selection.");
+    }
+}
+*/
--- test/common/util/math/myPowersTest.js
+++ test/common/util/math/myPowersTest.js
@@ -1,1 +1,57 @@
-//copy from ex7
+// NOTE this is following BDD
+
+// const assert = require('assert');
+// NOTE using chai for assertions instead of assert that comes with nodeJS 
+const assert = require('chai').assert;
+const myPowers = require('./../../../../src/common/util/math/myPowers');
+
+const num1 = 5;
+const num1String = "5";
+const num1NaN = "Hello";
+const num1Square = 25;
+const num1Cube = 125;
+
+describe('myPowers', function() {
+    describe('toSquare', function() {
+        it('toSquare(' + num1 + ') of myPowers should return the value of "' + num1Square + '"', function() {
+            let result = myPowers.toSquare(num1);
+            assert.equal(result, num1Square);
+        });
+
+        it('toSquare("' + num1String + '") of myPowers should return the value of "' + num1Square + '"', function() {
+            let result = myPowers.toSquare(num1String);
+            assert.equal(result, num1Square);
+        });
+
+        it('toSquare(' + num1NaN + ') of myPowers should return the value of "NaN"', function() {
+            let result = myPowers.toSquare(num1NaN);
+            assert.equal(result, "NaN");
+        });
+
+        it('toSquare(' + num1 + ') of myPowers should return type number', function() {
+            let result = myPowers.toSquare(num1);
+            assert.typeOf(result, 'number');
+        });
+    });
+    describe('toCube', function() {
+        it('toCube(' + num1 + ') of myPowers should return the value of "' + num1Cube + '"', function() {
+            let result = myPowers.toCube(num1);
+            assert.equal(result, num1Cube);
+        });
+
+        it('toCube("' + num1String + '") of myPowers should return the value of "' + num1Cube + '"', function() {
+            let result = myPowers.toCube(num1String);
+            assert.equal(result, num1Cube);
+        });
+
+        it('toCube(' + num1NaN + ') of myPowers should return the value of "NaN"', function() {
+            let result = myPowers.toCube(num1NaN);
+            assert.equal(result, "NaN");
+        });
+
+        it('toCube(' + num1 + ') of myPowers should return type number', function() {
+            let result = myPowers.toCube(num1);
+            assert.typeOf(result, 'number');
+        });
+    });
+});
